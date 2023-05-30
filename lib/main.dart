2bb968ca609c9f52b464635ac853d164de07056a
JeanCamargoReal <jeancamargoreal@icloud.com>
2023-05-30 13:19:06 -0300

    tornando membros privados

--- lib/main.dart
+++ lib/main.dart
@@ -3,25 +3,24 @@ import 'package:flutter/material.dart';
 main() => runApp(PerguntaApp());
 
 class PerguntaApp extends StatefulWidget {
-
   @override
-  State<PerguntaApp> createState() => PerguntaAppState();
+  State<PerguntaApp> createState() => _PerguntaAppState();
 }
 
-class PerguntaAppState extends State<PerguntaApp> {
-  var perguntaSelecionada = 0;
+class _PerguntaAppState extends State<PerguntaApp> {
+  var _perguntaSelecionada = 0;
 
-  void responder() {
+  void _responder() {
     setState(() {
-      perguntaSelecionada++;
+      _perguntaSelecionada++;
     });
 
-    print(perguntaSelecionada);
+    print(_perguntaSelecionada);
   }
 
   @override
   Widget build(BuildContext context) {
-    final List <String> perguntas = [
+    final List<String> perguntas = [
       'Qual é a sua cor favorita',
       'Qual é o seu animal favorito',
     ];
@@ -32,11 +31,14 @@ class PerguntaAppState extends State<PerguntaApp> {
           title: const Text('Perguntas'),
         ),
         body: Column(
-          children: <Widget> [
-            Text(perguntas[perguntaSelecionada]),
-            ElevatedButton(onPressed: responder, child: const Text('Resposta 1')),
-            ElevatedButton(onPressed: responder, child: const Text('Resposta 2')),
-            ElevatedButton(onPressed: responder, child: const Text('Resposta 3'))
+          children: <Widget>[
+            Text(perguntas[_perguntaSelecionada]),
+            ElevatedButton(
+                onPressed: _responder, child: const Text('Resposta 1')),
+            ElevatedButton(
+                onPressed: _responder, child: const Text('Resposta 2')),
+            ElevatedButton(
+                onPressed: _responder, child: const Text('Resposta 3'))
           ],
         ),
       ),
